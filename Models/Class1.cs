0face99ea45e59e3512df87b3adbb7dc293daf3d
宗畦 黃 <hsi040@hotmail.com>
2018-05-09 18:51:04 +0800

    新增ProductFormat DTO

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace BuildSchool.MvcSolution.OnlineStore.Models
 {
-    public class Class1
+    public class ProductFormat
     {
+        public int ProductFormatID { get; set; }
+        public int ProductID { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public int StockQuantity { get; set; }
     }
 }
