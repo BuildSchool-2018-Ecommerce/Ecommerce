e7ab379bf21936c071b407ee8b05bc2fbc239d85
宗畦 黃 <hsi040@hotmail.com>
2018-05-09 18:39:00 +0800

    新增ShoppingCarts DTO

---
+++ Models/ShoppingCarts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MvcSolution.OnlineStore.Models
+{
+    public class ShoppingCarts
+    {
+        public int ID { get; set; }
+        public string MemberID { get; set; }
+        public int ProductFormaatID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
