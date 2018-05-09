45be7c36c966277a9bec362a9e53748f81f895a2
宗畦 黃 <hsi040@hotmail.com>
2018-05-09 17:10:28 +0800

    修改Phone

--- Models/Members.cs
+++ Models/Members.cs
@@ -11,7 +11,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models
         public string MemberID { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
-        public int Phone { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
     }
